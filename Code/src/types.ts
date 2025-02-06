26a6fba7a409064eb9ad00d5b531ff696d80d9b3
Vladislav Maraev <vlad@maraev.me>
2025-02-06 20:35:33 +0100

    Lab2 and code sample

---
+++ Code/src/types.ts
@@ -0,0 +1,9 @@
+import { Hypothesis, SpeechStateExternalEvent } from "speechstate";
+import { AnyActorRef } from "xstate";
+
+export interface DMContext {
+  spstRef: AnyActorRef;
+  lastResult: Hypothesis[] | null;
+}
+
+export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
